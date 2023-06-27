00ff39c97202204dab44eaaf041d728eb4d809b0
Ivan Kos <kosenkov@Ivans-iMac.local>
2023-06-27 13:17:45 +0300

    Start Swyft hw 07

---
+++ Swyft_hw_07_pr1/Swyft_hw_07_pr1/Swyft_hw_07_pr1App.swift
@@ -0,0 +1,17 @@
+//
+//  Swyft_hw_07_pr1App.swift
+//  Swyft_hw_07_pr1
+//
+//  Created by Ivan Kosenkov on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swyft_hw_07_pr1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swyft_hw_07_pr2/Swyft_hw_07_pr2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swyft_hw_07_pr2
+//
+//  Created by Ivan Kosenkov on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
